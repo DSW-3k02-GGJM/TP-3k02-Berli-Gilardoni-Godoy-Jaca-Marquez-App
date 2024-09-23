52f0cae89b04e9aa659b7f18d80445ff40066338
juampi74 <juampijaca@gmail.com>
2024-09-23 20:28:56 -0300

    Primera version Check-in y Check-out Reserva

--- frontend/src/app/res-folder/res-form/res-form.component.ts
+++ frontend/src/app/res-folder/res-form/res-form.component.ts
@@ -4,46 +4,48 @@ import {
   FormGroup,
   ReactiveFormsModule,
   Validators,
-  AbstractControl
+  AbstractControl,
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { ApiService } from '../../service/api.service';
 import { ResCreatedOrModifiedService } from '../res-created-or-modified/res.service.js';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import {map, Observable} from "rxjs";
+import { map, Observable } from 'rxjs';
 //falta todo lo del service created-or-modified
 
 @Component({
   selector: 'app-res-form',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, ReactiveFormsModule,],
+  imports: [CommonModule, HttpClientModule, ReactiveFormsModule],
   templateUrl: './res-form.component.html',
-  styleUrl: './res-form.component.scss'
+  styleUrl: './res-form.component.scss',
 })
-export class ResFormComponent implements OnInit{
+export class ResFormComponent implements OnInit {
   @Input() title: string = '';
   @Input() currentResId: number = -1;
   action: string = '';
   clients: any[] = [];
   vehicles: any[] = [];
 
-
   constructor(
     private apiService: ApiService,
     private resCreatedOrModifiedService: ResCreatedOrModifiedService,
     public activeModal: NgbActiveModal,
-    private httpClient: HttpClient,
+    private httpClient: HttpClient
   ) {}
 
-  resForm = new FormGroup({
-    startDate: new FormControl('', [Validators.required]),
-    plannedEndDate: new FormControl('', [Validators.required]),
-    documentType: new FormControl('', [Validators.required]),
-    documentID: new FormControl('', [Validators.required]),
-    licensePlate: new FormControl('', [Validators.required]),
-  }, { validators: this.dateLessThan('startDate', 'plannedEndDate') });
-  
+  resForm = new FormGroup(
+    {
+      startDate: new FormControl('', [Validators.required]),
+      plannedEndDate: new FormControl('', [Validators.required]),
+      documentType: new FormControl('', [Validators.required]),
+      documentID: new FormControl('', [Validators.required]),
+      licensePlate: new FormControl('', [Validators.required]),
+    },
+    { validators: this.dateLessThan('startDate', 'plannedEndDate') }
+  );
+
   //valida que startDate >= fecha actual y valida que startDate < plannedEndDate
   dateLessThan(startDateField: string, endDateField: string) {
     return (formGroup: AbstractControl) => {
@@ -52,7 +54,10 @@ export class ResFormComponent implements OnInit{
       const today = new Date().toISOString().split('T')[0]; // Obtiene la fecha actual en formato YYYY-MM-DD
 
       if (startDate && new Date(startDate) < new Date(today)) {
-        formGroup.get(startDateField)?.setErrors({ dateInvalid: 'La fecha de inicio debe ser igual o mayor a la fecha actual' });
+        formGroup.get(startDateField)?.setErrors({
+          dateInvalid:
+            'La fecha de inicio debe ser igual o mayor a la fecha actual',
+        });
       } else if (startDate && endDate && startDate >= endDate) {
         formGroup.get(endDateField)?.setErrors({ dateInvalid: true });
       } else {
@@ -77,9 +82,7 @@ export class ResFormComponent implements OnInit{
       this.apiService
         .getOne('reservations', Number(this.currentResId))
         .subscribe((response) => {
-          let startDateFormat = this.formatDate(
-            response.data.startDate
-          );
+          let startDateFormat = this.formatDate(response.data.startDate);
           let plannedEndDateFormat = this.formatDate(
             response.data.plannedEndDate
           );
@@ -102,30 +105,19 @@ export class ResFormComponent implements OnInit{
     }
   }
 
-    formatDate(DateDB: string): string {
-      let DateFormat: string = '${year}-${month}-${day}';
-      DateFormat = DateFormat.replace(
-        '${year}',
-        DateDB.substring(0, 4)
-      );
-      DateFormat = DateFormat.replace(
-        '${month}',
-        DateDB.substring(5, 7)
-      );
-      DateFormat = DateFormat.replace(
-        '${day}',
-        DateDB.substring(8, 10)
-      );
-      return DateFormat;
-    }
+  formatDate(DateDB: string): string {
+    let DateFormat: string = '${year}-${month}-${day}';
+    DateFormat = DateFormat.replace('${year}', DateDB.substring(0, 4));
+    DateFormat = DateFormat.replace('${month}', DateDB.substring(5, 7));
+    DateFormat = DateFormat.replace('${day}', DateDB.substring(8, 10));
+    return DateFormat;
+  }
 
-   // Método para cargar los clientes
-   loadClients(): void {
+  // Método para cargar los clientes
+  loadClients(): void {
     this.apiService.getAll('clients').subscribe((response) => {
       this.clients = response.data;
     });
-
-
   }
 
   // Método para cargar los vehiculos
@@ -135,34 +127,35 @@ export class ResFormComponent implements OnInit{
     });
   }
 
-  onSubmit(){
+  onSubmit() {
     if (this.resForm.valid) {
-
       const formData = this.resForm.value;
 
       console.log('Patente seleccionada:', formData.licensePlate); // Para verificar qué patente se selecciona
 
-    // Encontrar el vehículo seleccionado: compara por patente ya que eso es lo que se elige del vehiculo en el select --> ?????
-    const selectedVehicle = this.vehicles.find(vehicle => vehicle.id === Number(formData.licensePlate));
+      // Encontrar el vehículo seleccionado: compara por patente ya que eso es lo que se elige del vehiculo en el select --> ?????
+      // const selectedVehicle = this.vehicles.find(
+      //   (vehicle) => vehicle.id === Number(formData.licensePlate)
+      // );
 
-    console.log('Vehículo seleccionado:', selectedVehicle); // Verificar si se encuentra el vehículo
+      // console.log('Vehículo seleccionado:', selectedVehicle); // Verificar si se encuentra el vehículo
 
-    if (!selectedVehicle) {
-      console.error('No se encontró el vehículo seleccionado');
-      return; // Detener si no se encuentra el vehículo
-    }
+      // if (!selectedVehicle) {
+      //   console.error('No se encontró el vehículo seleccionado');
+      //   return; // Detener si no se encuentra el vehículo
+      // }
       const finalData = {
         reservationDate: new Date().toISOString().split('T')[0],
         startDate: formData.startDate,
         plannedEndDate: formData.plannedEndDate,
         realEndDate: null,
         cancellationDate: null,
-        initialKms: selectedVehicle.totalKms,
+        initialKms: 0, // Revisar, debería ser null apenas se crea la reserva
         finalKm: null,
-        client: Number(formData.documentID), 
+        client: Number(formData.documentID),
         vehicle: Number(formData.licensePlate),
       };
-      
+
       /*
       const finalData = {
         ...formData,
@@ -173,9 +166,11 @@ export class ResFormComponent implements OnInit{
       this.activeModal.close();
 
       if (this.action === 'Create') {
-        this.apiService.create('reservations', finalData).subscribe((response) => {
-          this.resCreatedOrModifiedService.notifyResCreatedOrModified();
-        });
+        this.apiService
+          .create('reservations', finalData)
+          .subscribe((response) => {
+            this.resCreatedOrModifiedService.notifyResCreatedOrModified();
+          });
       } else if (this.action === 'Edit') {
         this.apiService
           .update('reservations', this.currentResId, finalData)
--- frontend/src/app/res-folder/res-table/res-table.component.ts
+++ frontend/src/app/res-folder/res-table/res-table.component.ts
@@ -7,7 +7,7 @@ import { ConfirmDeletionComponent } from '../../shared/confirm-deletion/confirm-
 import { FilterPipe } from '../../shared/filter/filter.pipe';
 import { FormsModule } from '@angular/forms';
 import { ResFormComponent } from '../res-form/res-form.component.js';
-
+import { ResCreatedOrModifiedService } from '../res-created-or-modified/res.service.js';
 
 @Component({
   selector: 'app-res-table',
@@ -28,22 +28,17 @@ export class ResTableComponent {
   @Output() resDeleted = new EventEmitter();
   filterRows = '';
 
-  constructor(private apiService: ApiService, private modalService: NgbModal) {}
+  constructor(
+    private apiService: ApiService,
+    private modalService: NgbModal,
+    private resCreatedOrModifiedService: ResCreatedOrModifiedService
+  ) {}
 
   formatDate(DateDB: string): string {
     let DateTable: string = '${day}/${month}/${year}';
-    DateTable = DateTable.replace(
-      '${month}',
-      DateDB.substring(5, 7)
-    );
-    DateTable = DateTable.replace(
-      '${day}',
-      DateDB.substring(8, 10)
-    );
-    DateTable = DateTable.replace(
-      '${year}',
-      DateDB.substring(0, 4)
-    );
+    DateTable = DateTable.replace('${month}', DateDB.substring(5, 7));
+    DateTable = DateTable.replace('${day}', DateDB.substring(8, 10));
+    DateTable = DateTable.replace('${year}', DateDB.substring(0, 4));
     return DateTable;
   }
 
@@ -74,5 +69,47 @@ export class ResTableComponent {
       () => {}
     );
   }
-}
 
+  checkInRes(res: any): void {
+    const modalRef = this.modalService.open(ConfirmDeletionComponent);
+    modalRef.componentInstance.title = 'Check-in Reserva';
+    modalRef.componentInstance.message = `¿Está seguro de que desea realizar el check-in de la reserva?`;
+
+    modalRef.result.then(
+      (result) => {
+        if (result) {
+          this.apiService
+            .update('reservations', Number(res.id), {
+              initialKms: res.vehicle.totalKms,
+            })
+            .subscribe((response) => {
+              this.resCreatedOrModifiedService.notifyResCreatedOrModified();
+            });
+        }
+      },
+      () => {}
+    );
+  }
+
+  checkOutRes(res: any): void {
+    const modalRef = this.modalService.open(ConfirmDeletionComponent);
+    modalRef.componentInstance.title = 'Check-out Reserva';
+    modalRef.componentInstance.message = `¿Está seguro de que desea realizar el check-out de la reserva?`;
+
+    modalRef.result.then(
+      (result) => {
+        if (result) {
+          this.apiService
+            .update('reservations', Number(res.id), {
+              realEndDate: new Date().toISOString().split('T')[0],
+              finalKm: res.vehicle.totalKms,
+            })
+            .subscribe((response) => {
+              this.resCreatedOrModifiedService.notifyResCreatedOrModified();
+            });
+        }
+      },
+      () => {}
+    );
+  }
+}
