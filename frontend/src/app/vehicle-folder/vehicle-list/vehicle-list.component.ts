783805d1dd81df5b2265f2929d2aacd488951e70
juampi74 <juampijaca@gmail.com>
2024-11-09 21:37:31 -0300

    Deshabilito botones de editar y eliminar reservas para las que ya fue realizado el Check-Out. Corrijo datos accedidos en la lista de vehiculos filtrados para que devuelva bien los vehículos disponibles

--- frontend/src/app/vehicle-folder/vehicle-list/vehicle-list.component.ts
+++ frontend/src/app/vehicle-folder/vehicle-list/vehicle-list.component.ts
@@ -1,11 +1,11 @@
-import {CommonModule, NgForOf} from '@angular/common';
-import {Component, Input, OnInit} from '@angular/core';
+import { CommonModule, NgForOf } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { VehicleCardComponent } from '../vehicle-card/vehicle-card.component';
 import { ApiService } from '../../service/api.service';
 import { VehicleFilterComponent } from '../vehicle-filter/vehicle-filter.component';
-import {NgbActiveModal, NgbModal} from "@ng-bootstrap/ng-bootstrap";
-import {filter} from "rxjs"; // Importa el componente de filtro
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { filter } from 'rxjs'; // Importa el componente de filtro
 
 @Component({
   selector: 'app-vehicle-list',
@@ -17,7 +17,8 @@ import {filter} from "rxjs"; // Importa el componente de filtro
     HttpClientModule,
     VehicleCardComponent,
     VehicleFilterComponent,
-    NgForOf],
+    NgForOf,
+  ],
   providers: [ApiService, NgbActiveModal, NgbModal],
 })
 export class VehicleListComponent implements OnInit {
@@ -27,21 +28,24 @@ export class VehicleListComponent implements OnInit {
   constructor(
     private apiService: ApiService,
     private http: HttpClient,
-    private modalService: NgbModal) {}
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit(): void {
     this.openFilterModal(false);
   }
 
-  openFilterModal(dismissable : boolean) {
+  openFilterModal(dismissable: boolean) {
     const modalRef = this.modalService.open(VehicleFilterComponent, {
       size: 'l',
       centered: true,
-      beforeDismiss: () => dismissable //para que no se pueda cerrar
+      beforeDismiss: () => dismissable, //para que no se pueda cerrar
     });
-    modalRef.componentInstance.filterApplied.subscribe((filter: { startDate: string; endDate: string; location: string }) => {
-      this.onFilterApplied(filter);
-    });
+    modalRef.componentInstance.filterApplied.subscribe(
+      (filter: { startDate: string; endDate: string; location: string }) => {
+        this.onFilterApplied(filter);
+      }
+    );
   }
 
   editFilter() {
@@ -49,28 +53,38 @@ export class VehicleListComponent implements OnInit {
   }
 
   fetchVehicles(filter: any) {
-    this.http.get<any>(`/api/vehicles/available?startDate=${filter.startDate}&endDate=${filter.endDate}&location=${filter.location}`).subscribe(response => {
-      console.log('Response data:', response);
-      this.response = response.data.map((vehicle: any) => {
-        console.log('Vehicle data:', vehicle);
-        return {
-          vehicleModel: vehicle.vehicleModel.vehicleModelName,
-          category: vehicle.vehicleModel.category.categoryName,
-          passengerCount: vehicle.vehicleModel.passengerCount,
-          image: vehicle.vehicleModel.imagePath,
-          pricePerDay: vehicle.vehicleModel.category.pricePerDay,
-          deposit: vehicle.vehicleModel.category.depositValue,
-        };
-      });
-      console.log('Mapped response:', this.response);
-    }, error => {
-      console.error('Error fetching vehicles:', error);
-    });
+    this.http
+      .get<any>(
+        `/api/vehicles/available?startDate=${filter.startDate}&endDate=${filter.endDate}&location=${filter.location}`
+      )
+      .subscribe(
+        (response) => {
+          console.log('Response data:', response);
+          this.response = response.data.map((vehicle: any) => {
+            console.log('Vehicle data:', vehicle);
+            return {
+              vehicleModel: vehicle.vehicleModelName,
+              category: vehicle.category.categoryName,
+              passengerCount: vehicle.passengerCount,
+              image: vehicle.imagePath,
+              pricePerDay: vehicle.category.pricePerDay,
+              deposit: vehicle.category.depositValue,
+            };
+          });
+          console.log('Mapped response:', this.response);
+        },
+        (error) => {
+          console.error('Error fetching vehicles:', error);
+        }
+      );
   }
 
-  onFilterApplied(filter: { startDate: string; endDate: string; location: string }) {
+  onFilterApplied(filter: {
+    startDate: string;
+    endDate: string;
+    location: string;
+  }) {
     const { startDate, endDate, location } = filter;
     this.fetchVehicles(filter);
   }
-  
 }
